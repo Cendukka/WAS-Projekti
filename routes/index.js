2463a185b0d0a918ebd8e999553085f7ce001499
Klaus Jokinen <36441457+KlausJokinen@users.noreply.github.com>
2019-12-09 14:53:25 +0200

    Some fixes

--- routes/index.js
+++ routes/index.js
@@ -4,30 +4,28 @@ const {Announcement} = require('../models/announcement');
 
 
 /* GET create page. */
-router.get('/create', function(req, res, next) {
-  const {error} = req.query;
-  const token = req.csrfToken();
-  console.log(req.csrfToken());
-  res.render('create', {error: error, token:token});
+router.get('/create', function (req, res, next) {
+    const {error} = req.query;
+    const token = req.csrfToken();
+    console.log(req.csrfToken());
+    res.render('create', {error: error, token: token});
 });
 
 
 /* GET create page. */
-router.get('/view', async function(req, res, next) {
-  const announcementsDocs = await Announcement.find({});
-  const announcements = announcementsDocs.map(announcement => {
-    console.log(announcement.expirationDate.toLocaleDateString())
-    return {
-      name: announcement.name,
-      email: announcement.email,
-      content: announcement.content,
-      category: announcement.category,
-      expirationDate: announcement.expirationDate.toLocaleDateString()
-    }
-  });
+router.get('/view', async function (req, res, next) {
+    const announcementsDocs = await Announcement.find({});
+    const announcements = announcementsDocs.map(announcement => {
+        return {
+            name: announcement.name,
+            email: announcement.email,
+            content: announcement.content,
+            category: announcement.category,
+            expirationDate: announcement.expirationDate.toLocaleDateString()
+        }
+    });
 
-
-  res.render('show', {announcements: announcements});
+    res.render('show', {announcements: announcements});
 });
 
 module.exports = router;
